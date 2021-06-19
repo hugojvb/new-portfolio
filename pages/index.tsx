8ade56b08fbe944f1e231665f006f585bfa60e0d
Hugo Batista <hugojvb20@gmail.com>
2021-06-19 22:23:14 +0100

    Deleted boiler plate and added simple landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,18 @@
-import Link from "next/link";
-import Layout from "../components/Layout";
+import Head from "next/head";
+import { Fragment } from "react";
 
 const IndexPage = () => (
-	<Layout title="Home | Next.js + TypeScript Example">
-		<h1 className="text-red-100">Hello Next.js 👋</h1>
-		<p>
-			<Link href="/about">
-				<a className="absolute text-light line-through">About</a>
-			</Link>
-		</p>
-	</Layout>
+	<Fragment>
+		<Head>
+			<title>Hugo Batista - Software Developer</title>
+		</Head>
+		<div className="min-h-screen flex flex-col items-center justify-center">
+			<h1 className="text-7xl text-yellow-300 antialiased font-thin">I am Hugo and I love what I do</h1>
+			<button className="text-3xl rounded-full border-solid border-2 border-yellow-300 p-3 font-thin text-yellow-300 mt-10 focus:outline-none hover:bg-gray-100">
+				Know More
+			</button>
+		</div>
+	</Fragment>
 );
 
 export default IndexPage;
