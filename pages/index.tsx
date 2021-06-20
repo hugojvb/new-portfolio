53be8c5b9772ce7fa270c31da3233b502c1616e1
Hugo Batista <hugojvb20@gmail.com>
2021-06-20 10:39:52 +0100

    ADded react reveal

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,21 @@
 import Head from "next/head";
-import { Fragment } from "react";
+import { Fragment, FC } from "react";
+import { Fade } from "react-reveal";
 
-const IndexPage = () => (
+const IndexPage: FC = () => (
 	<Fragment>
 		<Head>
 			<title>Hugo Batista - Software Developer</title>
 		</Head>
 		<div className="min-h-screen flex flex-col items-center justify-center">
-			<h1 className="text-7xl text-yellow-300 antialiased font-thin">I am Hugo and I love what I do</h1>
-			<button className="text-3xl rounded-full border-solid border-2 border-yellow-300 p-3 font-thin text-yellow-300 mt-10 focus:outline-none hover:bg-gray-100">
-				Know More
-			</button>
+			<Fade top>
+				<h1 className="text-7xl text-yellow-300 antialiased font-thin">I am Hugo and I love what I do</h1>
+			</Fade>
+			<Fade top delay={1000}>
+				<button className="text-3xl rounded-full border-solid border-2 border-yellow-300 p-3 font-thin text-yellow-300 mt-10 focus:outline-none hover:bg-gray-100">
+					More About Me
+				</button>
+			</Fade>
 		</div>
 	</Fragment>
 );
