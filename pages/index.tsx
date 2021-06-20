15c2edb3a8f7f528ea12dcc1118bf1e4bceb9f10
Hugo Batista <hugojvb20@gmail.com>
2021-06-20 11:03:24 +0100

    Added global css

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,16 @@ const IndexPage: FC = () => (
 			<Fade top>
 				<h1 className="text-7xl text-yellow-300 antialiased font-thin">I am Hugo and I love what I do</h1>
 			</Fade>
-			<Fade top delay={1000}>
-				<button className="text-3xl rounded-full border-solid border-2 border-yellow-300 p-3 font-thin text-yellow-300 mt-10 focus:outline-none hover:bg-gray-100">
+			<Fade top delay={500}>
+				<button
+					onClick={(e) => window.scrollBy({ top: window.innerHeight, left: 0, behavior: "smooth" })}
+					className="text-3xl rounded-full border-solid border-2 border-yellow-300 p-3 font-thin text-yellow-300 mt-10 focus:outline-none hover:bg-gray-100"
+				>
 					More About Me
 				</button>
 			</Fade>
 		</div>
+		<div className="min-h-screen flex flex-col items-center justify-center">sojikdboisurfb</div>
 	</Fragment>
 );
 
