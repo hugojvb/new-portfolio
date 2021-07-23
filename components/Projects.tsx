29fab4831f422ec8c60dbf12d2bc3df83d51701a
Hugo Batista <hugojvb20@gmail.com>
2021-07-23 21:43:14 +0100

    Added projects array of objects

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,21 +1,28 @@
 import { FC, Fragment } from "react";
 
+const projects: { name: string; description: string; videoURL: string }[] = [
+	{
+		name: "Fitbit Workout Tracker",
+		description:
+			"Developed a Progressive Web App that shows fitbit user data in a minimalistic and responsive grid UI. Built with React (Context, Hooks, Suspense) and the Fitbit API. Deployed with Netlify.",
+		videoURL: "/fitbit.mp4",
+	},
+];
+
 // FUNCTIONAL COMPONENT
 const Projects: FC = () => {
 	return (
-		<div className="min-h-screen flex flex-col items-center bg-secondary mt-40">
+		<div className="min-h-screen flex flex-col items-center bg-secondary mt-40 relative">
 			<h2 className="text-6xl text-white mb-24 mt-32 font-thin">My Projects</h2>
-			<div className="flex justify-around items-center h-96">
-				<div className="w-1/2 p-20">
-					<video></video>
-				</div>
-
-				<h4 className="w-1/2 text-white m-20 p-20 flex items-center justify-center">
-					Ex eu officia irure esse velit esse. Quis enim ipsum reprehenderit do fugiat ad est fugiat aute. Officia pariatur Lorem nisi ea
-					enim excepteur laborum laboris magna amet. Incididunt tempor nisi irure pariatur et ullamco dolore sit consequat quis nulla ea. In
-					irure laboris est cillum proident nostrud consequat sunt amet aliqua et nulla. Incididunt et sunt ipsum qui officia amet.
-				</h4>
-			</div>
+			{projects.map((project) => {
+				<div className="flex justify-around items-center h-96">
+					<h3>{project.name}</h3>
+					<div className="w-1/2 p-20">
+						<video src={project.videoURL}></video>
+					</div>
+					<h4 className="w-1/2 text-white m-20 p-20 flex items-center justify-center">{project.description}</h4>
+				</div>;
+			})}
 		</div>
 	);
 };
