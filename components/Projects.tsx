d26e953814ddaba9cd29c005c23e458df97d75eb
Hugo Batista <hugojvb20@gmail.com>
2021-07-24 16:05:14 +0100

    Changed projects top divider

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -8,9 +8,9 @@ import { projects } from "../utils/projects";
 const Projects: FC = (): JSX.Element => {
 	return (
 		<div className="min-h-screen flex flex-col items-center bg-secondary mt-24 pb-20 relative">
-			<div className="custom-shape-divider-top-1627074170 absolute w-full top-0 left-0 overflow-hidden leading-none rotate-180">
+			<div className="absolute w-full top-0 rotate-180">
 				<svg
-					className="block h-40px invert"
+					className="invert h-20 w-full"
 					data-name="Layer 1"
 					xmlns="http://www.w3.org/2000/svg"
 					viewBox="0 0 1200 120"
@@ -23,22 +23,22 @@ const Projects: FC = (): JSX.Element => {
 				</svg>
 			</div>
 			<Fade top>
-				<h2 className="text-6xl text-white mb-24 mt-32 font-thin">My Projects</h2>
+				<h2 className="text-6xl text-tertiary mb-24 mt-32 font-thin">My Projects</h2>
 			</Fade>
 			{projects.map((project) => {
 				return (
-					<div className="flex flex-col justify-around text-white p-20">
+					<div className="flex flex-col justify-around text-tertiary p-20">
 						<Fade top>
 							<h2 className="text-3xl font-thin mb-8">{project.name}</h2>
 						</Fade>
 
 						<div className="flex justify-around items-center h-96 text-white">
 							<div className="w-1/2 p-20">
-								<video controls autoPlay muted>
+								<video controls autoPlay muted loop>
 									<source src={project.videoURL} type="video/mp4" />
 								</video>
 							</div>
-							<h4 className="w-1/2 text-white p-20 leading-10">{project.description}</h4>
+							<h4 className="w-1/2 text-tertiary p-20 leading-10">{project.description}</h4>
 						</div>
 					</div>
 				);
