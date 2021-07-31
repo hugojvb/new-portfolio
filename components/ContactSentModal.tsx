611bff2876f91b816b630a922dbb6f9c22cc4a10
Hugo Batista <hugojvb20@gmail.com>
2021-07-31 13:01:25 +0100

    Changed responsiveness of modal

--- components/ContactSentModal.tsx
+++ components/ContactSentModal.tsx
@@ -7,10 +7,10 @@ const ContactSentModal: FC<{ setShowModal: Dispatch<SetStateAction<boolean>>; se
 	return sendSuccess ? (
 		<>
 			<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ease-linear transition-all duration-150">
-				<div className="relative w-auto my-6 mx-auto max-w-3xl">
+				<div className="relative w-5/6 md:w-auto my-6 mx-auto max-w-3xl">
 					<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 						<div className="flex items-start justify-center p-5 border-b border-solid border-gray-100 rounded-t">
-							<h3 className="text-3xl font-thin">Message sent successfully</h3>
+							<h3 className="text-3xl font-thin text-center">Message sent successfully</h3>
 						</div>
 						<div className="relative p-6 mb-8 flex-auto text-center flex flex-col items-center">
 							<img src="/feedback/checkmark.svg" alt="checkmark" className="w-28 my-8" />
@@ -35,10 +35,10 @@ const ContactSentModal: FC<{ setShowModal: Dispatch<SetStateAction<boolean>>; se
 	) : (
 		<>
 			<div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ease-linear transition-all duration-150">
-				<div className="relative w-auto my-6 mx-auto max-w-3xl">
+				<div className="relative w-5/6 md:w-auto my-6 mx-auto max-w-3xl">
 					<div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
 						<div className="flex items-start justify-center p-5 border-b border-solid border-gray-100 rounded-t">
-							<h3 className="text-3xl font-thin">Something went wrong</h3>
+							<h3 className="text-3xl font-thin text-center">Something went wrong</h3>
 						</div>
 						<div className="relative p-6 mb-8 flex-auto text-center flex flex-col items-center">
 							<img src="/feedback/error.svg" alt="error" className="w-28 my-8" />
