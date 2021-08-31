172c9d61b78e986162bfc2d2d7849895bafb602a
Hugo Batista <hugojvb20@gmail.com>
2021-08-31 23:34:14 +0100

    Changed hero section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,9 @@
 import dynamic from "next/dynamic";
 
 // ANIMATE ON SCROLL
-const ScrollAnimation = dynamic(() => import("react-animate-on-scroll"), { ssr: false });
+const ScrollAnimation = dynamic(() => import("react-animate-on-scroll"), {
+	ssr: false,
+});
 
 // SCROLL INTO ABOUT SECTION
 const scrollToNextSection = () => {
@@ -12,10 +14,14 @@ const scrollToNextSection = () => {
 const Hero = (): JSX.Element => {
 	return (
 		<header className="min-h-screen flex flex-col items-center justify-center">
-			<ScrollAnimation animateOnce animateIn="animate__fadeInDown">
-				<h1 className="text-5xl lg:text-7xl text-secondary antialiased font-thin text-center">I am Hugo and I love what I do</h1>
-			</ScrollAnimation>
-			<ScrollAnimation animateOnce animateIn="animate__fadeInDown" delay={500}>
+			<h1 className="text-5xl lg:text-7xl text-secondary antialiased font-thin text-center">
+				I am Hugo and I love what I do
+			</h1>
+			<ScrollAnimation
+				animateOnce
+				animateIn="animate__fadeInDown"
+				delay={500}
+			>
 				<button
 					onClick={scrollToNextSection}
 					className="custom-fill text-2xl lg:text-3xl rounded-full border-solid border-2 border-primary p-3 font-thin text-secondary mt-10 focus:outline-none"
